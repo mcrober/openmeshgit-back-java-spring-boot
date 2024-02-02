8dc6ca8fc6c65d48a72645ecd5c3b6a436e7899b
mcrober <157477326+mcrober@users.noreply.github.com>
2024-02-02 12:18:52 +0100

    Add files via upload

---
+++ src/main/java/org/barmanyrober/openmeshgit/OpenmeshgitApplication.java
@@ -0,0 +1,13 @@
+package org.barmanyrober.openmeshgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmeshgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenmeshgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/barmanyrober/openmeshgit/OpenmeshgitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.barmanyrober.openmeshgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenmeshgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
